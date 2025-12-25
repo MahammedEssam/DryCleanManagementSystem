ad72fc923e2f38070006b98567d35539b6bd883e
MahammedEssam <mahammedessam552@gmail.com>
2025-12-25 21:25:25 +0200

    Add files via upload

---
+++ DAL/DatabaseConnection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+public static class DatabaseConnection
+{
+    private readonly static string _connectionString;
+
+    public static DatabaseConnection()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        var configuration = builder.Build();
+        _connectionString = configuration.GetConnectionString("DryCleanDB");
+
+        if (string.IsNullOrEmpty(_connectionString))
+            throw new Exception("Connection string is empty! تحقق من appsettings.json");
+    }
+
+    // ترجع Connection String بدل SqlConnection
+    public static string ConnectionString()
+    {
+        return _connectionString;
+    }
+}
