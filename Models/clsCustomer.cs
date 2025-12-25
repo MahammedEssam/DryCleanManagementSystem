2a371a46ec939771d8006d221c662214a86398ce
MahammedEssam <mahammedessam552@gmail.com>
2025-12-25 21:15:40 +0200

    Add files via upload

---
+++ Models/clsCustomer.cs
@@ -0,0 +1,12 @@
+﻿namespace DryCleanShopApi.DAL.Models
+{
+    public class clsCustomer
+    {
+        public int CustomerID { get; set; }              // رقم العميل
+        public string Name { get; set; } = default!;                // اسم العميل
+        public string? Phone { get; set; }               // رقم الهاتف (nullable)
+        public string? Address { get; set; }             // العنوان (nullable)
+        public DateTime CreatedAt { get; set; }          // تاريخ الإنشاء
+        public string CreatedBy { get; set; } = default!;
+    }
+}
---
+++ Models/clsUser.cs
@@ -0,0 +1,18 @@
+﻿namespace DryCleanShopApi.DAL.Models
+{
+    public class clsUser
+    {
+        public int? UserID { get; set; }              // رقم المستخدم
+        public string Username { get; set; } = default!;   // اسم الدخول
+        public string Name { get; set; } = default!;       // الاسم الحقيقي
+        public string Phone { get; set; } = default!;      // رقم الهاتف
+        public string PasswordHash { get; set; } = default!; // كلمة المرور مشفرة
+        public int? Role { get; set; }                // رقم الدور (Admin, Worker, ...)
+        public DateTime CreatedAt { get; set; }      // تاريخ الإنشاء
+        public int? CreatedBy { get; set; }           // المستخدم اللي أنشأ الحساب
+
+        // خصائص إضافية للعرض
+        public string? RoleName { get; set; }        // اسم الدور (من جدول Roles)
+        public string? CreatedByUsername { get; set; } // اسم المستخدم اللي أنشأ الحساب
+    }
+}
